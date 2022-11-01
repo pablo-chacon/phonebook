03a54d8808afa2c11b6f421f2d35980faf414f99
rodercode <rodersnow@gmail.com>
2022-11-01 13:12:09 +0100

    hogaboga

--- src/Application.java
+++ src/Application.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
 
+        System.out.println("Hogaboga");
 
 //        Contact contact = new Contact();
 //        Scanner sc = new Scanner(System.in);
