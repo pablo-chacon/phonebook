d6de3cd2f5c139905cb329df729c0b804cac4f78
ivanaorz <113606494+ivanaorz@users.noreply.github.com>
2022-11-01 13:24:20 +0100

    Create Administrator.java

---
+++ src/Administrator.java
@@ -0,0 +1,56 @@
+public class Administrator implements Crud {
+
+    private String userName;
+    private String password;
+    private boolean isLoggedIn;
+
+    public Administrator(String userName, String password, Boolean isLoggedIn) {
+    this.userName = userName;
+    this.password = password;
+    this.isLoggedIn = isLoggedIn;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isLoggedIn() {
+        return isLoggedIn;
+    }
+
+    public void setLoggedIn(boolean loggedIn) {
+        isLoggedIn = loggedIn;
+    }
+
+    @Override
+    public void createProfile() {
+
+    }
+
+    @Override
+    public void readProfile() {
+
+    }
+
+    @Override
+    public void updateProfile() {
+
+    }
+
+    @Override
+    public void deleteProfile() {
+
+    }
+}
